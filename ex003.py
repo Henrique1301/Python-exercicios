f139f6a647051a7adb816db83b34b1d6b339f9c0
Henrique1301 <73301553+Henrique1301@users.noreply.github.com>
2021-03-31 15:45:39 -0300

    Criação do ex003.py

---
+++ ex003.py
@@ -0,0 +1,7 @@
+print('==='*45)
+print('Soma entre dois números')
+print('==='*45)
+n1 = int(input('Digite um valor:'))
+n2 = int(input('Digite outro valor:'))
+s = n1+n2
+print('{} + {} = {}'.format(n1, n2, s))
