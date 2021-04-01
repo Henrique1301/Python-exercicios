d73a6e1c0f7b8bb42ffd5413008e6a6de24432b8
Henrique1301 <73301553+Henrique1301@users.noreply.github.com>
2021-04-01 17:11:37 -0300

    Creating ex005.py

---
+++ ex005.py
@@ -0,0 +1,5 @@
+print('==='*40)
+print('Antecessor e sucessor de um número')
+print('==='*40)
+n = int(input('Digite um número?'))
+print('O antecessor de {} é {}\nO sucessor de {} é {}'.format(n,n-1,n,n+1))
