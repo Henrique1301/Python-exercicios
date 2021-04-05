6a3dbcbc3422e206a6516c3dfc6caa52c87ec724
Henrique1301 <73301553+Henrique1301@users.noreply.github.com>
2021-04-05 15:47:04 -0300

    Renaming the ex008.py

--- ex008_(Conversor de M para cm e mm).py
+++ ex008_(Conversor de M para cm e mm).py
@@ -3,5 +3,5 @@ print('Conversor de M para cm e mm')
 print('==='*40)
 m = float(input('Digite o valor em Metros:'))
 cm = 100*m
-mm= 1000*m
+mm = 1000*m
 print('{}M corresponde á {}cm e {}mm'.format(m,cm,mm))
