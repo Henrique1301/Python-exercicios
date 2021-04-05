c7846c515fc5aaa44cc081ec48757895b3b2fe79
Henrique1301 <73301553+Henrique1301@users.noreply.github.com>
2021-04-05 15:21:42 -0300

    Creating ex006.py

---
+++ ex006.py
@@ -0,0 +1,10 @@
+from math import sqrt
+print('===' * 45)
+print('Dobro, triblo e raiz de um número')
+print('===' * 45)
+n1 = int(input('Digite um número:'))
+dobro = n1*2
+triplo = n1*3
+raiz = sqrt(n1)
+print('O dobro de {} é {}\nO triplo de {} é {}\nA raiz de {} é {}'.format(n1,n1*2,n1,n1*3,n1,sqrt(n1)))
+
