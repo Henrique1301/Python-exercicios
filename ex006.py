ef7021d13d698dc2be0a0d440ca702ae21f264c1
Henrique1301 <73301553+Henrique1301@users.noreply.github.com>
2021-04-05 15:23:22 -0300

    Update the ex006.py

--- ex006.py
+++ ex006.py
@@ -6,5 +6,5 @@ n1 = int(input('Digite um número:'))
 dobro = n1*2
 triplo = n1*3
 raiz = sqrt(n1)
-print('O dobro de {} é {}\nO triplo de {} é {}\nA raiz de {} é {}'.format(n1,n1*2,n1,n1*3,n1,sqrt(n1)))
+print('O dobro de {} é {}\nO triplo de {} é {}\nA raiz de {} é {}'.format(n1,dobro,n1,triplo,n1,raiz))
 
